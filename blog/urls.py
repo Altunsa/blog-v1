8dd7d7e76e5e8ced90099061d9fd0acd6251b887
unknown <georgekattae@gmail.com>
2025-03-16 22:03:26 -0400

    publication update-done

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import (PublicationListView, 
+from .views import (
+                    PublicationListView, 
                     PublicationDetailView,
-                    PublicationCreateView
+                    PublicationCreateView,
+                    PublicationUpdateView
                     )
 
 
@@ -9,4 +11,5 @@ urlpatterns = [
     path('', PublicationListView.as_view(), name='publications-list'), 
     path('publication/<int:pk>/', PublicationDetailView.as_view(), name='publication-detail'),
     path('publication/new/', PublicationCreateView.as_view(), name='publication-create'), 
+    path('publication/<int:pk>/update/', PublicationUpdateView.as_view(), name='publication-update'),
 ]
